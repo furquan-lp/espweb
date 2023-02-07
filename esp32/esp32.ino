4be47ecd136f1b41b3a9024d7e150add168b140f
Syed F Ahmad <furquan@protonmail.com>
2023-02-07 16:24:48 +0530

    Count the number of WiFi attempts and abort after setting LED_ONBOARD HIGH if they exceed 100 (10 seconds)

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -19,9 +19,18 @@ void setup() {
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi");
     digitalWrite(LED_PIN, HIGH);
+    uint8_t wifi_attempt = 0;
     while (WiFi.status() != WL_CONNECTED) {
+        if (wifi_attempt >= 100) {
+            Serial.println("Couldn't connect to WiFi. Aborting...");
+            digitalWrite(LED_PIN, LOW);
+            digitalWrite(LED_ONBOARD, HIGH);
+            delay(1000);
+            return;
+        }
         delay(100);
         Serial.print(".");
+        wifi_attempt++;
     }
     digitalWrite(LED_PIN, LOW);
     Serial.printf("\nConnected at %s\n", WiFi.localIP().toString().c_str());
