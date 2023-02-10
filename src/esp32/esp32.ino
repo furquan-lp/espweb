8dfa015d6b05cac5ebe54a494c5e51314fa8d284
Syed F Ahmad <furquan@protonmail.com>
2023-02-11 02:49:38 +0530

    Removed webserver handler and globals since they're now managed by the espweb-common library

--- src/esp32/esp32.ino
+++ src/esp32/esp32.ino
@@ -1,26 +1,24 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
-
-#include "esp32web.h"
-
-AsyncWebServer server(80);
+#include <espweb.h>
+#include <espweb_common.h>
 
 void setup() {
     Serial.begin(115200);
     Serial.setDebugOutput(true);
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(LED_ONBOARD, OUTPUT);
+    pinMode(ESP32_LED_PIN, OUTPUT);
+    pinMode(ESP32_LED_ONBOARD, OUTPUT);
 
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi");
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(ESP32_LED_PIN, HIGH);
     uint8_t wifi_attempt = 0;
     while (WiFi.status() != WL_CONNECTED) {
         if (wifi_attempt >= 100) {
             Serial.println("Couldn't connect to WiFi. Aborting...");
-            digitalWrite(LED_PIN, LOW);
-            digitalWrite(LED_ONBOARD, HIGH);
+            digitalWrite(ESP32_LED_PIN, LOW);
+            digitalWrite(ESP32_LED_ONBOARD, HIGH);
             delay(1000);
             esp_deep_sleep_start();
             return;
@@ -29,7 +27,7 @@ void setup() {
         Serial.print(".");
         wifi_attempt++;
     }
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(ESP32_LED_PIN, LOW);
     Serial.printf("\nConnected at %s\n", WiFi.localIP().toString().c_str());
 
     server.on("/", HTTP_GET, handle_webserver_root);
@@ -37,11 +35,3 @@ void setup() {
 }
 
 void loop() { delay(100); }
-
-void handle_webserver_root(AsyncWebServerRequest* request) {
-    uint32_t seconds = millis() / 1000;
-    const char* format_str = "Hello World!\nSeconds since boot: %d\n";
-    char server_str[sizeof(format_str) + 64];
-    sprintf(server_str, format_str, seconds);
-    request->send(200, "text/plain", server_str);
-}
