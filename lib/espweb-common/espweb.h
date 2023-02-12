ee0372ca2e61b77a7e9c3635dccc62182cf9a7dc
Syed F Ahmad <furquan@protonmail.com>
2023-02-12 23:02:42 +0530

    Added handle_webserver_json

--- lib/espweb-common/espweb.h
+++ lib/espweb-common/espweb.h
@@ -5,6 +5,7 @@
 
 void handle_webserver_root(AsyncWebServerRequest* request);
 void handle_webserver_style(AsyncWebServerRequest* request);
+void handle_webserver_json(AsyncWebServerRequest* request);
 void update_server_json_data(const char* ipaddr, uint32_t free_heap);
 void blink_led(uint8_t led_pin, bool invert);
 
