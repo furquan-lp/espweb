7a44d97396486251f3cf0c6de90b3f1e7ad96779
Syed F Ahmad <furquan@protonmail.com>
2023-02-13 00:58:34 +0530

    Update cpu and flash data as well in updateData

--- src/web/app.js
+++ src/web/app.js
@@ -8,7 +8,7 @@ setInterval(() => {
 }, 2000);
 
 const getAll = () => {
-  axios.get('data')
+  axios.get('http://192.168.0.8/data')
     .then(function (response) {
       console.log(response.data);
       jsonObject = response.data;
@@ -19,4 +19,6 @@ const updateData = (data) => {
   document.getElementById("uptime").innerHTML = data.uptime;
   document.getElementById("ip").innerHTML = data.ipaddr;
   document.getElementById("heap").innerHTML = data.free_heap;
+  document.getElementById("cpu").innerHTML = data.cpu;
+  document.getElementById("flash").innerHTML = data.flash;
 }
